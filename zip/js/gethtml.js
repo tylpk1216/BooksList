8be6c6f1e614396ad9eb9685d3f875166060c592
tylpk1216 <tylpk1216@gmail.com>
2019-05-07 11:50:41 +0800

    Modify flow.

--- zip/js/gethtml.js
+++ zip/js/gethtml.js
@@ -1,5 +1,94 @@
-chrome.runtime.sendMessage({
-    action: "getSource",
-    source: document.documentElement.innerHTML,
-    url: document.URL
-});
+function sendResponse(items) {
+    chrome.runtime.sendMessage({
+        action: "getSource",
+        source: items,
+        url: document.URL
+    });
+}
+
+let bookStores = [
+    {
+        name: 'readmoo',
+        func: showReadmooBooks
+    },
+    {
+        name: 'kobo',
+        func: showKoboBooks
+    },
+    {
+        name: 'google',
+        func: showGoogleBooks
+    }
+];
+
+function showReadmooBooks(source) {
+    let regex = /<div class=\"title\" title=\"(.*)\">/g;
+    showBook(regex, source);
+}
+
+function showKoboBooks(source) {
+    let regex = /<p class=\"title product-field.*>\n.*>(.*)</g;
+    showBook(regex, source);
+}
+
+function showBook(regex, source) {
+    let info;
+    let items = [];
+    while ((info = regex.exec(source)) != null) {
+        items.push(info[1]);
+    }
+
+    sendResponse(items);
+}
+
+function showGoogleBooks(source) {
+    let startPattern = 'href="/books/reader?id=';
+    let namePattern = 'title="';
+
+    let newSource = '';
+    while (true) {
+        let i = source.indexOf(startPattern);
+        if (i === -1) break;
+
+        source = source.slice(i + startPattern.length);
+
+        let next_i = source.indexOf(namePattern);
+        if (next_i === -1) break;
+
+        source = source.slice(next_i + namePattern.length);
+
+        let last_i = source.indexOf('"');
+        if (last_i === -1) break;
+
+        let name = source.slice(0, last_i);
+        newSource += name + '\n';
+
+        source = source.slice(last_i + newSource.length + 1);
+
+        // some books need to be adjusted
+        let extraPattern = '> ' + name;
+        let extra_i = source.indexOf(extraPattern);
+        if (extra_i !== -1) {
+            source = source.slice(extra_i + extraPattern.length);
+        }
+    }
+
+    let regex = /(.*)\n/g;
+    showBook(regex, newSource);
+}
+
+function checkBookStore() {
+    let url = document.URL;
+    let source = document.documentElement.innerHTML;
+
+    for (let i = 0; i < bookStores.length; i++) {
+        if (url.indexOf(bookStores[i].name) >= 0) {
+            bookStores[i].func(source);
+            return;
+        }
+    }
+
+    sendResponse([]);
+}
+
+checkBookStore();
--- zip/js/popup.js
+++ zip/js/popup.js
@@ -2,21 +2,6 @@ const messageID = '#message';
 
 let message = null;
 
-let bookStores = [
-    {
-        name: 'readmoo',
-        func: showReadmooBooks
-    },
-    {
-        name: 'kobo',
-        func: showKoboBooks
-    },
-    {
-        name: 'google',
-        func: showGoogleBooks
-    }
-];
-
 function setMessage(strLen, msg) {
     if (message) {
         message.style.width = strLen * 15 + 'px';
@@ -37,67 +22,6 @@ function getNumberString(len, index) {
     return s;
 }
 
-function showBook(regex, source) {
-    let info;
-    let msg = '';
-    let index = 1;
-    let strLen = 0;
-    while ((info = regex.exec(source)) != null) {
-        if (info[1].length > strLen) strLen = info[1].length;
-
-        msg += getNumberString(3, index) + '\t\t\t' + info[1] + '\n';
-        index++;
-    }
-
-    setMessage(strLen, msg);
-}
-
-function showReadmooBooks(source) {
-    let regex = /<div class=\"title\" title=\"(.*)\">/g;
-    showBook(regex, source);
-}
-
-function showKoboBooks(source) {
-    let regex = /<p class=\"title product-field.*>\n.*>(.*)</g;
-    showBook(regex, source);
-}
-
-function showGoogleBooks(source) {
-    let startPattern = 'href="/books/reader?id=';
-    let namePattern = 'title="';
-
-    let newSource = '';
-    while (true) {
-        let i = source.indexOf(startPattern);
-        if (i === -1) break;
-
-        source = source.slice(i + startPattern.length);
-
-        let next_i = source.indexOf(namePattern);
-        if (next_i === -1) break;
-
-        source = source.slice(next_i + namePattern.length);
-
-        let last_i = source.indexOf('"');
-        if (last_i === -1) break;
-
-        let name = source.slice(0, last_i);
-        newSource += name + '\n';
-
-        source = source.slice(last_i + newSource.length + 1);
-
-        // some books need to be adjusted
-        let extraPattern = '> ' + name;
-        let extra_i = source.indexOf(extraPattern);
-        if (extra_i !== -1) {
-            source = source.slice(extra_i + extraPattern.length);
-        }
-    }
-
-    let regex = /(.*)\n/g;
-    showBook(regex, newSource);
-}
-
 function onWindowLoad() {
     message = document.querySelector(messageID);
     chrome.tabs.executeScript(null, {
@@ -110,15 +34,25 @@ function onWindowLoad() {
     });
 }
 
+function showMessage(items) {
+    let msg = '';
+    let index = 1;
+    let strLen = 0;
+
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].length > strLen) strLen = items[i].length;
+
+        msg += getNumberString(3, index) + '\t\t\t' + items[i] + '\n';
+        index++;
+    }
+
+    setMessage(strLen, msg);
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
-        for (let i = 0; i < bookStores.length; i++) {
-            if (request.url.indexOf(bookStores[i].name) >= 0) {
-                bookStores[i].func(request.source);
-                return;
-            }
-        }
-        message.innerText = 'not supported yet';
+        let items = request.source;
+        showMessage(items);
     }
 });
 
