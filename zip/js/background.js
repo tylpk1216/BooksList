916dd260e07faaba5d43146888f6c8547b95438e
tylpk1216 <tylpk1216@gmail.com>
2019-05-07 11:49:51 +0800

    Simplify code.

--- zip/js/background.js
+++ zip/js/background.js
@@ -2,21 +2,14 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([
             {
-                conditions: [new chrome.declarativeContent.PageStateMatcher({
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
                         pageUrl: {hostEquals: 'new-read.readmoo.com'},
-                    })
-                ],
-                actions: [new chrome.declarativeContent.ShowPageAction()]
-            },
-            {
-                conditions: [new chrome.declarativeContent.PageStateMatcher({
+                    }),
+                    new chrome.declarativeContent.PageStateMatcher({
                         pageUrl: {hostEquals: 'www.kobo.com'},
-                    })
-                ],
-                actions: [new chrome.declarativeContent.ShowPageAction()]
-            },
-            {
-                conditions: [new chrome.declarativeContent.PageStateMatcher({
+                    }),
+                    new chrome.declarativeContent.PageStateMatcher({
                         pageUrl: {hostEquals: 'play.google.com'},
                     })
                 ],
